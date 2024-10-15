e355d92529083ce5fc6517706de206c01a6b8991
Ariiq Yusuf <yusufariiq3@gmail.com>
2024-10-15 13:13:26 +0700

    using env variables for query

--- src/repositories/lpjRepository.js
+++ src/repositories/lpjRepository.js
@@ -1,9 +1,12 @@
+require('dotenv').config();
 const pool = require('../config/db');
+
+const LPJ_TABLE_NAME = process.env.PG_TABLE_NAME
 
 class LpjRepository {
     async saveLpj(noRequest, tglLpj, filePath) {
       const query = `
-        INSERT INTO lpj_history (no_request, tgl_lpj, file_path)
+        INSERT INTO ${LPJ_TABLE_NAME} (no_request, tgl_lpj, file_path)
         VALUES ($1, $2, $3)
         RETURNING id
       `;
@@ -13,13 +16,13 @@ class LpjRepository {
     }
   
     async getLpjHistory() {
-      const query = 'SELECT * FROM lpj_history ORDER BY created_at DESC';
+      const query = 'SELECT * FROM ${LPJ_TABLE_NAME} ORDER BY created_at DESC';
       const result = await pool.query(query);
       return result.rows;
     }
   
     async getLpjById(id) {
-      const query = 'SELECT file_path FROM lpj_history WHERE id = $1';
+      const query = 'SELECT file_path FROM ${LPJ_TABLE_NAME} WHERE id = $1';
       const result = await pool.query(query, [id]);
       return result.rows[0];
     }
